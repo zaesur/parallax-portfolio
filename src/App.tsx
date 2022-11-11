2c4edf1518878eb8e696a0766e9767d701c3eb10
Florian Van Belleghem <fvbe@stylelabs.com>
2022-11-11 14:57:00 +0100

    Set up starter pack

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience";
 import "./App.css";
+import { PerspectiveCamera } from "@react-three/drei";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <div className="webgl">
+        <Canvas>
+          <PerspectiveCamera
+            makeDefault
+            fov={35}
+            near={0.1}
+            far={100}
+            position={[0, 0, 6]}
+          />
+          <Experience />
+        </Canvas>
+      </div>
+
+      <section className="section">
+        <h1>My Portfolio</h1>
+      </section>
+      <section className="section">
+        <h2>My projects</h2>
+      </section>
+      <section className="section">
+        <h2>Contact me</h2>
+      </section>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Experience.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from "react";
+
+const Experience: FunctionComponent = () => (
+  <mesh>
+    <boxGeometry args={[1, 1, 1]} />
+    <meshBasicMaterial color="#ff0000" />
+  </mesh>
+);
+
+export default Experience;
